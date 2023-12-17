8dee8f36992d57c3b4d5501cd3dd433da10fac6f
Nina Doroshenko <nina.doroshenko20@gmail.com>
2023-12-17 20:15:28 +0300

    created server

---
+++ game_shop/settings.py
@@ -0,0 +1,17 @@
+# Global settings
+DEBUG = True
+
+# Database settings
+DB_PATH = '../game_shop/src/server/database'
+DB_NAME = 'game_shop.db'
+
+# Server settings
+
+APP = 'server:app'
+HOST = '127.0.0.1'
+PORT = 8000
+URL = f'http://{HOST}:{PORT}'
+
+# Client settings
+
+IMG_DIR = '../game_shop/resources/img'
